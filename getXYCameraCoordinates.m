0ebb4a9235b2c9c1065b7b4f9e6f36a20541a8ca
David <davidwichter@yahoo.de>
2017-04-18 15:48:24 +0200

    Deleted files

--- getXYCameraCoordinates.m
+++
@@ -1,9 +0,0 @@
-function [ cameraCoordinates ] = getXYCameraCoordinates(imagePoints, handles)
-%GETCAMERACOORDINATES Summary of this function goes here
-%   Detailed explanation goes here
-R = eye(3,3);
-t = [0 0 1];
-cameraCoordinates = pointsToWorld(handles.cameraParams, R, t, imagePoints);
-%test = inv(handles.cameraParams.IntrinsicMatrix') * [imagePoints(1,:) 1]';
-end
-

ba613aa5fc95663cde0a3967df33c52ef1cb5d63
David <davidwichter@yahoo.de>
2017-05-23 18:06:58 +0200

    Progress

--- ComputeAverageDistanceToRectangle.m
+++ ComputeAverageDistanceToRectangle.m
@@ -5,6 +5,6 @@ distances = zeros(length(pointCloud), 1)';
 for i = 1:length(pointCloud)
    distances(i) = GetDistanceToRectangle(pointCloud(i,:), rectanglePoints);
 end
-distance = median(distances);
+distance = mean(distances);
 end
 
--- extractLineFromMaskedImage.m
+++ extractLineFromMaskedImage.m
@@ -1,26 +1,26 @@
 function [ points ] = extractLineFromMaskedImage( maskedImage, unmaskedImage )
 [rows, cols, channels] = size(maskedImage);
-grayScaleMask = rgb2gray(maskedImage);
 points = [];
+tempImage = maskedImage;
 for c = 1:cols
     mu = 0;
     summedWeights = 0;
     for r = 1:rows
-        weight = double(grayScaleMask(r, c));
+        weight = double(maskedImage(r, c, 1));
         mu = mu + r * weight;  
         summedWeights = summedWeights + weight;
     end
     mu = mu / summedWeights;
     
    
-    if mu > 0
-        %points2 = [points2; c max(mu, 0)];
-        
+    if mu > 11 && mu < 710
         preliminaryLaserMiddle = max(mu, 0);
+        
+        tempImage(floor(preliminaryLaserMiddle) - 10 : floor(preliminaryLaserMiddle) + 10, c, 1) = unmaskedImage(floor(preliminaryLaserMiddle) - 10 : floor(preliminaryLaserMiddle) + 10, c, 1);
         
         mu = 0;
         summedWeights = 0;
-        for r = floor(preliminaryLaserMiddle) - 10 : floor(preliminaryLaserMiddle) + 10
+        for r = (floor(preliminaryLaserMiddle) - 10) : (floor(preliminaryLaserMiddle) + 10)
             weight = double(unmaskedImage(r, c, 1));
             mu = mu + r * weight;  
             summedWeights = summedWeights + weight;

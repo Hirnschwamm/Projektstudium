e4a8673ea434f67633ce6d5fddf2e34230aa8bac
David <davidwichter@yahoo.de>
2017-05-24 21:16:49 +0200

    detailed figurine data

--- PlotPointCloud.m
+++ PlotPointCloud.m
@@ -2,7 +2,7 @@ function [ ] = PlotPointCloud( pc )
 %GETPOINTCLOUDANDPLOTIT Summary of this function goes here
 %   Detailed explanation goes here
 plot3(pc(:,1), pc(:,2), pc(:,3), '.');
-axis([0 190 0 130 -120 0]);
+axis([0 190 -30 130 -120 0]);
 set(gca,'Zdir','reverse');
 set(gca,'Ydir','reverse');
 xlabel('X');
